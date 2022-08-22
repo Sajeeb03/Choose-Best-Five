f6c03b17d29ce94a8d3388931ba36d8bcd9fb3e4
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-08-22 15:51:39 +0600

    selected five list

---
+++ js/list.js
@@ -0,0 +1,60 @@
+document.getElementById('btn-1').addEventListener('click', function () {
+    listOfSelectedFive('player-1');
+    makeButtonDisabled('btn-1');
+
+})
+document.getElementById('btn-2').addEventListener('click', function () {
+    listOfSelectedFive('player-2');
+    makeButtonDisabled('btn-2');
+
+})
+document.getElementById('btn-3').addEventListener('click', function () {
+    listOfSelectedFive('player-3');
+    makeButtonDisabled('btn-3');
+
+})
+document.getElementById('btn-4').addEventListener('click', function () {
+    listOfSelectedFive('player-4');
+    makeButtonDisabled('btn-4');
+
+})
+document.getElementById('btn-5').addEventListener('click', function () {
+    listOfSelectedFive('player-5');
+    makeButtonDisabled('btn-5');
+
+})
+document.getElementById('btn-6').addEventListener('click', function () {
+    listOfSelectedFive('player-6');
+    makeButtonDisabled('btn-6');
+
+})
+document.getElementById('btn-7').addEventListener('click', function () {
+    listOfSelectedFive('player-7');
+    makeButtonDisabled('btn-7');
+
+})
+document.getElementById('btn-8').addEventListener('click', function () {
+    listOfSelectedFive('player-8');
+    makeButtonDisabled('btn-8');
+
+})
+document.getElementById('btn-9').addEventListener('click', function () {
+    listOfSelectedFive('player-9');
+    makeButtonDisabled('btn-9');
+
+})
+document.getElementById('btn-10').addEventListener('click', function () {
+    listOfSelectedFive('player-10');
+    makeButtonDisabled('btn-10');
+
+})
+document.getElementById('btn-11').addEventListener('click', function () {
+    listOfSelectedFive('player-11');
+    makeButtonDisabled('btn-11');
+
+})
+document.getElementById('btn-12').addEventListener('click', function () {
+    listOfSelectedFive('player-12');
+    makeButtonDisabled('btn-12');
+
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,32 @@
-console.log('Hello from js')
+function getPlayerNames(elementId) {
+    const playerNameFields = document.getElementById(elementId);
+    const playerName = playerNameFields.innerText;
+    return playerName;
+}
+
+function listOfSelectedFive(elementId) {
+    const playerName = getPlayerNames(elementId);
+    const listField = document.getElementById('list-items');
+
+    const listItemNumbers = document.querySelectorAll('li');
+    const numbers = listItemNumbers.length;
+
+    if (numbers <= 4) {
+        let listItem = document.createElement('li');
+        listItem.innerText = playerName;
+        listField.appendChild(listItem);
+    }
+    else {
+        alert('You can not buy more than 5');
+        return;
+    }
+
+
+}
+
+function makeButtonDisabled(elementId) {
+    const buttonField = document.getElementById(elementId);
+    buttonField.setAttribute('disabled', true);
+}
+
+
